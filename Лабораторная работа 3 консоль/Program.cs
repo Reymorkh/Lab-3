d900cb88d6cdd85ed76c4d425cab55ad1d6427b6
Reymorkh <aareimorkh@edu.hse.ru>
2023-10-18 16:17:18 +0500

    Я понял?!?!?

--- Лабораторная работа 3 консоль/Program.cs
+++ Лабораторная работа 3 консоль/Program.cs
@@ -26,56 +26,43 @@ namespace Лабораторная_работа_3_консоль
             Console.WriteLine("Условие задачи: y равен 3 в степени x");
             Console.WriteLine("диапазон аргумента: 0,1 <= x <= 1; k = 0.09; n = 10, e = 0.0001");
             Console.WriteLine("s = 1 + (Ln(3)/1!) * x ... (Math.Pow(Ln(3), n) / n!) * (Math.Pow(x, n)");
-            Console.WriteLine();
 
 
             for (double x = a; x <= b; x += k)
             {
-                Console.Write("X= {0} ", Math.Round(x, 4));
+                Console.Write("X= {0} ",Math.Round(x, 4));
 
-                int f = 1, fTemp = 1, nTemp = 0; ;
-                double lastResult = 1, logResult = 1, xResult = x, summTemp=1;
-
-                do
-                {
-                    logResult *= Math.Log(3);
-                    f *= fTemp;
-                    fTemp++;
-                    xResult *= x;
-                    nTemp++;
-                    lastResult = logResult / f * xResult;
-                }
-                while (lastResult != e);
-                Console.WriteLine(nTemp);
-                string fdsda = Console.ReadLine();
-
+                int fResult = 1, fTemp = 1;
+                double lastResult = 1, logResult = 1, xResult = 1, summTemp=1;                
 
                 for (int i = 1; i <= n; i++)
                 {
 
                     logResult *= Math.Log(3);
-                    f *= fTemp;
+                    fResult *= fTemp;
                     fTemp++;
                     xResult *= x;
 
-                    double tempResult = logResult / f * xResult; //вычисление куска рекуррента
-                    
+                    double tempResult = logResult / fResult * xResult; //вычисление куска рекуррента                    
                     SN += tempResult; //итоговая переменная для общей суммы
-
-                    if (lastResult - tempResult < e) //итоговая переменная для точности?
-                        SE += tempResult;
-
-                    lastResult = tempResult;
-
-
-
-                    
-
-                    
-
                 }
+
+                logResult = 1;
+                fResult= 1;
+                fTemp = 1;
+                xResult = 1;
+                do
+                {
+                    logResult *= Math.Log(3);
+                    fResult *= fTemp;
+                    fTemp++;
+                    xResult *= x;
+                    lastResult = logResult / fResult * xResult;
+                    SE += lastResult;
+                }
+                while (lastResult < e);
                 y = Math.Pow(3, x);
-                Console.WriteLine("SN= {0} SE= {1} Y= {2}", SN, SE, y);
+                Console.WriteLine("SN= {0} SE= {1} Y= {2}", SN, SE, Math.Round(y,4));
             }           
         }
     }
