07fe922d246c3030edf2052f60912cd7f9e0c022
Reymorkh <aareimorkh@edu.hse.ru>
2023-11-19 22:44:49 +0500

    Ещё слегка почистил код

--- Лабораторная работа 3 консоль/Program.cs
+++ Лабораторная работа 3 консоль/Program.cs
@@ -1,22 +1,23 @@
-﻿using System.Numerics;
-using static System.Net.WebRequestMethods;
-
-namespace Лабораторная_работа_3_консоль
+﻿namespace Лабораторная_работа_3_консоль
 {
   internal class Program
   {
     static void Main(string[] args)
     {
+      Console.BackgroundColor = ConsoleColor.White;
+      Console.ForegroundColor = ConsoleColor.Black;
+      Console.Clear();
+
       const double a = 0.1, b = 1, k = (b - a) / 10, e = 0.0001;
       const int n = 10;
       double SN = 1, SE = 1, y;
+      int counter;
       Console.Write("Условие задачи: y равен 3 в степени x\nдиапазон аргумента: 0,1 <= x <= 1; k = 0.09; n = 10, e = 0.0001\n");
       Console.WriteLine("s = 1 + (Ln(3)/1!) * x ... (Math.Pow(Ln(3), n) / n!) * (Math.Pow(x, n)\n");
 
-      int counter1;
       for (double x = a; x <= b; x += k)
       {
-        counter1 = 0;
+        counter = 0;
         Console.Write($"X= {x:f4} ");
 
         int factorialResult = 1, factorialTemp = 1;
@@ -32,9 +33,9 @@ namespace Лабораторная_работа_3_консоль
           SN += lastResult; //вычисление для заданной длины
           if (lastResult > e)
             SE += lastResult; //вычисление степенного ряда для заданной точности (e)
-          counter1++;
+          counter++;
         }
-        while (lastResult > e || counter1 != n);
+        while (lastResult > e || counter != n);
 
         y = Math.Pow(3, x);
         Console.WriteLine($"SN= {SN:f4} SE= {SE:f4} Y= {y:f4}");
