a96b1e0d7072bdc05a27712a6ad269396abde1c9
Reymorkh <aareimorkh@edu.hse.ru>
2023-11-23 16:21:57 +0500

    Я врал прежде, но не в этот раз

--- Лабораторная работа 3 консоль/Program.cs
+++ Лабораторная работа 3 консоль/Program.cs
@@ -12,42 +12,42 @@
       const int n = 10;
       Console.Write("Условие задачи: y равен 3 в степени x\nдиапазон аргумента: 0,1 <= x <= 1; k = 0.09; n = 10, e = 0.0001\n");
       Console.WriteLine("s = 1 + (Ln(3)/1!) * x ... (Math.Pow(Ln(3), n) / n!) * (Math.Pow(x, n)\n");
-
       for (double x = a; x <= b; x += k)
       {
         double SN = 1, SE = 1, y;
-        Console.Write($"X= {x:f4} ");
 
         int factorialResult = 1, factorialTemp = 1;
-        double lastResult = 1, logResult = Math.Log(3), localLogResult = 1, xResult = 1;
+        double lastResult = 1, log = Math.Log(3), localLogResult = 1, xResult = 1;
+
         for (int i = 1; i <= n; i++) //вычисление степенного ряда с известной длиной (n)
         {
-          localLogResult *= logResult;
+          localLogResult *= log;
           factorialResult *= i;
           xResult *= x;
-          SN += logResult / (double)factorialResult * xResult; //вычисление куска рекуррента //итоговая переменная для общей суммы
+          SN += localLogResult / (double)factorialResult * xResult; //вычисление куска рекуррента //итоговая переменная для общей суммы
         }
 
         localLogResult = 1;
         factorialResult = 1;
-        xResult = x;
-
+        xResult = 1;
         double tempResult;
+
         do //вычисление степенного ряда для заданной точности (e)
         {
           tempResult = lastResult;
-          localLogResult *= logResult;
+          localLogResult *= log;
           factorialResult *= factorialTemp;
-          factorialTemp++;
           xResult *= x;
           lastResult = localLogResult / (double)factorialResult * xResult;
           SE += lastResult;
+          factorialTemp++;
         }
         while (Math.Abs(tempResult - lastResult) > e);
-        SE -= lastResult;
+
         y = Math.Pow(3, x);
-        //Console.Write($"X= {x:f4} ");
-        Console.WriteLine($"SN= {SN:f4} SE= {SE:f4} Y= {y:f4}");
+
+        Console.WriteLine($"X= {x:f4} SN= {SN:f4} SE= {SE:f4} Y= {y:f4}");
+        //Console.Write(" счётчик прохода для цикла SE: " + counter);
       }
     }
   }
